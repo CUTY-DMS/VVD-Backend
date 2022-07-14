4c124b89d6c035e5aaea22335d65878c6059d262
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-14 12:10:31 +0900

    🐛 :: currentTime ->dateTime 수정

--- DMS-Todolist/src/main/java/com/project/dmstodolist/entity/todolist/Todo.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/entity/todolist/Todo.java
@@ -26,10 +26,11 @@ public class Todo {
     @Column(nullable = false)
     private String content;
 
-    private LocalDateTime currentTime;
+    private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
 
 }
