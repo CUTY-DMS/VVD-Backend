ac11d59a281d4a9a327e90a3cbf16a0736405795
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-17 18:28:22 +0900

    🌧 :: UserFacade 수정

--- DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
@@ -1,6 +1,7 @@
 package com.project.dmstodolist.facade;
 
 import com.project.dmstodolist.entity.user.User;
+import com.project.dmstodolist.security.auth.AuthDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ public class UserFacade {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if(authentication == null || authentication.getPrincipal() == null
-                || !((authentication.getPrincipal()) instanceof  User))
+                || !((authentication.getPrincipal()) instanceof AuthDetails))
             return null;
 
 
