bac7d01fe664910cfe525de44d5416da4a93ac27
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-18 00:28:03 +0900

    🌧 :: UserFacade 수정

--- DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
@@ -1,22 +1,31 @@
 package com.project.dmstodolist.facade;
 
 import com.project.dmstodolist.entity.user.User;
+import com.project.dmstodolist.entity.user.UserRepository;
+import com.project.dmstodolist.exception.TokenInvalidException;
+import com.project.dmstodolist.exception.UserNotFoundException;
 import com.project.dmstodolist.security.auth.AuthDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class UserFacade {
 
+    private final UserRepository userRepository;
+
     public User getUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if(authentication == null || authentication.getPrincipal() == null
-                || !((authentication.getPrincipal()) instanceof AuthDetails))
-            return null;
+        Object detail = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+        if(!(detail instanceof UserDetails)) {
+            throw new TokenInvalidException();
+        }
 
-        return (User) authentication.getPrincipal();
+        return userRepository.findByAccountId(((UserDetails) detail).getUsername())
+                .orElseThrow(UserNotFoundException::new);
     }
 }
--- DMS-Todolist/src/main/java/com/project/dmstodolist/security/auth/AuthDetails.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/security/auth/AuthDetails.java
@@ -3,16 +3,10 @@ package com.project.dmstodolist.security.auth;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.FetchType;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @RequiredArgsConstructor
