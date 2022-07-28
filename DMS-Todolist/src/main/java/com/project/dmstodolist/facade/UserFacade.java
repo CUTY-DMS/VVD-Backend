58228855226589f9b01aca3260ce4d1ab5ff3dc0
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-28 18:53:41 +0900

    ♻️ :: UserFacade

--- DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/facade/UserFacade.java
@@ -1,20 +1,14 @@
 package com.project.dmstodolist.facade;
 
 import com.project.dmstodolist.entity.user.User;
-import com.project.dmstodolist.entity.user.UserRepository;
 import com.project.dmstodolist.exception.AuthenticationNotFoundException;
-import com.project.dmstodolist.exception.UserNotFoundException;
 import com.project.dmstodolist.security.auth.AuthDetails;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class UserFacade {
-
-    private final UserRepository userRepository;
 
     public User getUser() {
 
@@ -26,5 +20,5 @@ public class UserFacade {
 
         return ((AuthDetails)principal).getUser();
     }
-    
+
 }
