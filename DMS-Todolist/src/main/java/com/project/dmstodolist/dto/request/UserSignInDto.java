e70a1b96f20f4be7a52dee525682c987083b7a28
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-29 21:53:11 +0900

    ♻️ :: 유효성

--- DMS-Todolist/src/main/java/com/project/dmstodolist/dto/request/UserSignInDto.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/dto/request/UserSignInDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,12 +11,10 @@ import javax.validation.constraints.Size;
 public class UserSignInDto {
 
     @NotNull
-    @Size(max = 20, message = "아이디는 최대 20글자")
     @JsonProperty("account_id")
     private String accountId;
 
     @NotNull
-    @Size(min = 8, max = 20, message = "비밀번호는 최소 8글자 최대 20글자")
     private String password;
 
 }
