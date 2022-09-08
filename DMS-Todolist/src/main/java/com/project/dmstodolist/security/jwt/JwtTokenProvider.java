48b16e807da745e73249b5d0d95a7d9e868c689d
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-08 19:00:57 +0900

    🌧 :: JwtTokenProvider

--- DMS-Todolist/src/main/java/com/project/dmstodolist/security/jwt/JwtTokenProvider.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,7 @@
 package com.project.dmstodolist.security.jwt;
 
+import com.project.dmstodolist.entity.refreshtoken.RefreshToken;
+import com.project.dmstodolist.entity.refreshtoken.RefreshTokenRepository;
 import com.project.dmstodolist.exception.ExpiredTokenException;
 import com.project.dmstodolist.exception.InvalidTokenException;
 import io.jsonwebtoken.*;
@@ -11,31 +13,58 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
 import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
+    private final JwtProperties jwtProperties;
     private final UserDetailsService userDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    private String secretKey = "token";
-
-    private long tokenValidTime = 30 * 60 * 1000L;
-
-    protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    public String generateAccessToken(String id) {
+        return generateToken(id, "access", jwtProperties.getAccessExp());
     }
 
+    public String generateRefreshToken(String id) {
+        String refreshToken = generateToken(id, "refresh", jwtProperties.getRefreshExp());
+        refreshTokenRepository.save(RefreshToken.builder()
+                .id(id)
+                .refreshToken(refreshToken)
+                .ttl(jwtProperties.getRefreshExp())
+                .build());
+
+        return refreshToken;
+    }
+
+    private String generateToken(String id, String type, Long exp) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .setSubject(id)
+                .claim("type", type)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
+                .compact();
+    }
 
     public String createToken(String accountId) {
         return Jwts.builder()
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1800000))
                 .setSubject(accountId)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(jwtProperties.getHeader());
+
+        if(bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix())
+                && bearerToken.length() > 7) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 
     public Authentication getAuthentication(String token) {
@@ -43,16 +72,10 @@ public class JwtTokenProvider {
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public String resolveToken(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-
-        return request.getHeader("Authorization");
-    }
-
     private Claims getBody(String token) {
         try {
             return Jwts.parser()
-                    .setSigningKey(secretKey)
+                    .setSigningKey(jwtProperties.getSecretKey())
                     .parseClaimsJws(token)
                     .getBody();
         } catch (ExpiredJwtException e) {
@@ -66,9 +89,3 @@ public class JwtTokenProvider {
     }
 
 }
-
-
-
-
-
-
