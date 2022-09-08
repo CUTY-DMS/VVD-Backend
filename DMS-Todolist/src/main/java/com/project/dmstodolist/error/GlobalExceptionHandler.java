66358aec4b6e2a85d66408ff952652989103c4fd
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-08 18:59:07 +0900

    🌧 :: GlobalExceptionHandler

--- DMS-Todolist/src/main/java/com/project/dmstodolist/error/GlobalExceptionHandler.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.project.dmstodolist.error;
 import com.project.dmstodolist.error.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,10 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ExceptionResponse> handlerException(BusinessException e) {
+    public ResponseEntity<ExceptionResponse> handlerException(BusinessException e) {
         final ErrorCode errorCode = e.getErrorCode();
 
         return new ResponseEntity<>(new ExceptionResponse(errorCode.getStatus(),
                 errorCode.getMessage()), HttpStatus.valueOf(errorCode.getStatus()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(
+                new ExceptionResponse(400, e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
 }
