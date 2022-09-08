d8a1a3f342a0374002af9c39b32ed9ad7e71c776
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-08 12:49:13 +0900

    🌧 :: Filter

--- DMS-Todolist/src/main/java/com/project/dmstodolist/security/jwt/JwtAuthenticationFilter.java
+++ DMS-Todolist/src/main/java/com/project/dmstodolist/security/jwt/JwtAuthenticationFilter.java
@@ -1,28 +1,27 @@
-package com.project.dmstodolist.security;
+package com.project.dmstodolist.security.jwt;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
+    public void doFilterInternal(HttpServletRequest request,
+                         HttpServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
 
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken(request);
 
         if(token != null) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
